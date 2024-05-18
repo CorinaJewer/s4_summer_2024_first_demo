1444d7844de10e821fe4bb871a95d11c06d87cee
Corina Jewer <corina.jewer@keyin.com>
2024-05-18 10:47:08 -0230

    Added Driver class

---
+++ src/main/java/com/keyn/Driver.java
@@ -0,0 +1,16 @@
+package com.keyn;
+
+public class Driver {
+
+    private String Name;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+
+
+}
